5ac9030b2b14cc792a055324c7556b2a637ae9f6
jminkoh <jminkoh@finamgabon.com>
2025-01-14 16:57:53 +0100

    Changement de la façon de se connecter

--- cloture/urls.py
+++ cloture/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.user_login, name='user_login'),
+    path('', views.login, name='login'),
     path('agences/', views.liste_agences, name='liste_agences'),
     path('agences/consulter_rapports', views.consulter_rapports, name='liste_rapports'),
     path('save_report/', views.save_report, name='save_report'),
